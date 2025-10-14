7c08778a3dfe367ef2213324eef0c2a659a9278e
polyface <polyface@proton.me>
2025-10-14 22:17:20 +0200

    continued server

--- server/src/libraria-server.cr
+++ server/src/libraria-server.cr
@@ -4,7 +4,7 @@ require "sqlite3"
 VERSION        = 0.1
 PORT_KEY       = "LIBRARIA_PORT"
 DB_KEY_PATH    = "LIBRARIA_DB_PATH"
-API_KEY_HEADER = "x-auth"
+API_KEY_HEADER = "X-Auth"
 DB_AUTH_TABLE  = "api_keys"
 
 ENV[PORT_KEY] ||= "9821"
@@ -14,23 +14,28 @@ KEY_LENGTH           = 11
 PORT                 = ENV[PORT_KEY].to_i
 SQUEAL_LIGHT_DB_PATH = ENV[DB_KEY_PATH]
 
-first_boot = File.exists?(SQUEAL_LIGHT_DB_PATH)
-
 db = DB.open "sqlite3://#{SQUEAL_LIGHT_DB_PATH}"
 
 db.exec "
   CREATE TABLE IF NOT EXISTS #{DB_AUTH_TABLE} (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     key TEXT UNIQUE,
-    label TEXT,
     created_at DATETIME DEFAULT (datetime('now')),  -- current timestamp
     last_used DATETIME,                             -- nullable
     revoked INTEGER DEFAULT 0                       -- 0 = false, 1 = true
 );
  "
-
 base_key = random_string(KEY_LENGTH)
 puts "The key for the server is #{base_key}"
+
+key_exists = false
+db.query("SELECT key FROM #{DB_AUTH_TABLE}") do |rs|
+  key_exists = rs.move_next
+end
+
+unless key_exists
+  db.exec "INSERT INTO #{DB_AUTH_TABLE} (key) VALUES (?) ", base_key
+end
 
 # Auth middleware
 class AuthMiddleware < Kemal::Handler
@@ -38,9 +43,26 @@ class AuthMiddleware < Kemal::Handler
   end
 
   def call(context)
-    key = context.response.headers[API_KEY_HEADER]
-    rs = @db.exec("SELECT key FROM #{DB_AUTH_TABLE} WHERE key = ?", key)
-    call_next context
+    key = context.request.headers[API_KEY_HEADER]?
+
+    unless key
+      context.response.status_code = 401
+      context.response.print "Missing API key"
+      return
+    end
+
+    found = false
+
+    @db.query("SELECT key FROM #{DB_AUTH_TABLE} WHERE key = ?", key) do |rs|
+      found = rs.move_next
+    end
+
+    if found
+      call_next context
+    else
+      context.response.status_code = 403
+      context.response.print "Forbidden"
+    end
   end
 end
 
